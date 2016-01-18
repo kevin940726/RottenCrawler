0c136b0975586c171880266c2f2644a1aa655051
kevin940726 <kevin830726@gmail.com>
2016-01-18 22:43:26 +0800

    critics

--- sentiment.py
+++ sentiment.py
@@ -70,24 +70,53 @@ def extract_features(document):
     return features
 
 
+def save_most_informative_features(self, n=10):
+    strlist = []
+    # Determine the most relevant features, and display them.
+    cpdist = self._feature_probdist
+    strlist.append('Most Informative Features')
+
+    for (fname, fval) in self.most_informative_features(n):
+        def labelprob(l):
+            return cpdist[l, fname].prob(fval)
+
+        labels = sorted([l for l in self._labels
+                         if fval in cpdist[l, fname].samples()],
+                        key=labelprob)
+        if len(labels) == 1:
+            continue
+        l0 = labels[0]
+        l1 = labels[-1]
+        if cpdist[l0, fname].prob(fval) == 0:
+            ratio = 'INF'
+        else:
+            ratio = '%8.1f' % (cpdist[l1, fname].prob(fval) /
+                               cpdist[l0, fname].prob(fval))
+        strlist.append(('%24s = %-14r %6s : %-6s = %s : 1.0' %
+               (fname, fval, ("%s" % l1)[:6], ("%s" % l0)[:6], ratio)))
+
+    return strlist
+
 def main():
     with open(path, 'r') as data_file:
         movieList = json.load(data_file)
 
-    all_reviews = getAllReviews(movieList)
+    all_reviews = getAllReviews(movieList[0:80])
     word_features = get_word_features(get_words_in_tweets(all_reviews))
 
     training_set = [({word: (word in x[0]) for word in word_features}, x[1]) for x in all_reviews]
     classifier = nltk.NaiveBayesClassifier.train(training_set)
 
-    all_critics = getAllCritics(movieList)
+    all_critics = getAllReviews(movieList[80:100])
     testing_set = [({word: (word in x[0]) for word in word_features}, x[1]) for x in all_critics]
-    most_informative = classifier.show_most_informative_features(100)
+    most_informative = save_most_informative_features(classifier, 100)
     accuracy = nltk.classify.util.accuracy(classifier, testing_set)
     print accuracy
 
-    with open("most_informative.txt", 'w+') as outfile:
-        outfile.write(most_informative)
+    with open("most_informative_reviews.txt", 'w+') as outfile:
+        outfile.write("%f\n" % accuracy)
+        for feature in most_informative:
+            outfile.write("%s\n" % feature)
 
 if __name__ == "__main__":
     main()
